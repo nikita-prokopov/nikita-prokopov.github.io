95fbfbe3f705fb701830fb51712cb3f64716cfb6
nikita-prokopov <61139889+nikita-prokopov@users.noreply.github.com>
2020-07-02 15:12:06 +0300

    Add files via upload

---
+++ sendsay-console-ui/precache-manifest.69770b9e377c15c57f1aca76866b7aad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa1d5030a4e62209b0af1e011db0d719",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.f0f0ca06ae2fd7978cda.bundle.js"
+  },
+  {
+    "url": "runtime~main.f0f0ca06ae2fd7978cda.bundle.js"
+  },
+  {
+    "revision": "e3c78be200b08bb40dd4",
+    "url": "static/css/main.22b6ffb6.chunk.css"
+  },
+  {
+    "revision": "b60017629da250ca90d8b767e9247181",
+    "url": "static/media/align-right.b6001762.svg"
+  },
+  {
+    "revision": "54d8dbc0fb2c398ade827b3be730629c",
+    "url": "static/media/colon.54d8dbc0.svg"
+  },
+  {
+    "revision": "e0c567bba2ae106b95744b4ef8009ab7",
+    "url": "static/media/log-out.e0c567bb.svg"
+  },
+  {
+    "revision": "aeb66b10d516e02c84324e3bfa1b8e4d",
+    "url": "static/media/meh-face.aeb66b10.svg"
+  },
+  {
+    "revision": "b9d973b0e8bddd1a38bc4c8317bacedd",
+    "url": "static/media/remove-fullscreen.b9d973b0.svg"
+  },
+  {
+    "revision": "11ebfce3a0b1c1c81fedbd31d75314d2",
+    "url": "static/media/set-fullscreen.11ebfce3.svg"
+  },
+  {
+    "url": "vendors~main.f0f0ca06ae2fd7978cda.bundle.js"
+  },
+  {
+    "url": "vendors~main.f0f0ca06ae2fd7978cda.bundle.js.LICENSE.txt"
+  }
+]);
